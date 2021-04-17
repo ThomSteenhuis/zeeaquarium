173eccc0e5a7dd22e45c2a1448a81496158f379f
Thom Steenhuis <thom.steenhuis@outlook.com>
2021-04-17 11:10:15 +0200

    added new sensor for ambient temperature

---
+++ ambienttemperature.py
@@ -0,0 +1,29 @@
+import os
+import logging
+import time
+
+import sensor_repo as sr
+import utils
+        
+CONTEXT = "ambienttemperature"
+
+os.system("modprobe w1_gpio")
+os.system("modprobe w1_therm")
+
+utils.setup_logging(CONTEXT)
+repo = sr.sensor_repo()
+
+try:
+    while True:
+        temp = utils.read_temp(1, "[" + CONTEXT + "]")
+
+        if temp and temp > 10 and temp < 40 :
+            repo.set_value(CONTEXT, temp)
+        else:
+            logging.warning(f"[{CONTEXT}] invalid measurement")
+        
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    repo.close_connection()
