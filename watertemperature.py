e79430dc5cf96b991a00a0dab121b440a487af67
Thom Steenhuis <thom.steenhuis@outlook.com>
2022-02-13 12:46:59 +0100

    added second water temperature sensor

--- watertemperature.py
+++ watertemperature.py
@@ -15,12 +15,25 @@ repo = sr.sensor_repo()
 
 try:
     while True:
-        temp = utils.read_temp(0, "[" + CONTEXT + "]")
-
-        if temp and temp > 15 and temp < 35:
+        temps = []
+        temp1 = utils.read_temp(0, "[" + CONTEXT + "]")
+        temp2 = utils.read_temp(2, "[" + CONTEXT + "]")
+        
+        if temp1 is None or temp1 < 15 or temp1 > 35:
+            logging.warning(f"[{CONTEXT}] invalid measurement of water temperature sensor 1")
+        else:
+            temps.append(temp1)
+            
+        if temp2 is None or temp2 < 15 or temp2 > 35:
+            logging.warning(f"[{CONTEXT}] invalid measurement of water temperature sensor 2")
+        else:
+            temps.append(temp2)
+        
+        if len(temps) > 0:
+            temp = sum(temps) / len(temps)
             utils.retry_if_none(lambda : repo.set_value(CONTEXT, temp))
         else:
-            logging.warning(f"[{CONTEXT}] invalid measurement")
+            logging.warning(f"[{CONTEXT}] no valid measurement")
         
         time.sleep(1)
 except KeyboardInterrupt:
