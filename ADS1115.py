a1d6abb95009cf9638483e22772fb51fc0963a35
Thom Steenhuis <thom.steenhuis@outlook.com>
2023-03-05 16:49:15 +0100

    Float sensor is now using ads1115

--- ADS1115.py
+++ ADS1115.py
@@ -4,9 +4,15 @@ class ADS1115:
     def __init__(self):
         self.adc1 = ADS.ADS1115(address=0x48)
         self.adc2 = ADS.ADS1115(address=0x49)
+        self.adc3 = ADS.ADS1115(address=0x4a)
+        self.adc4 = ADS.ADS1115(address=0x4b)
     
     def read(self, channel = 0, gain = 1):
         if channel < 4:
             return self.adc1.read_adc(channel, gain)
-        else:
+        elif channel < 8:
             return self.adc2.read_adc(channel - 4, gain)
+        elif channel < 12:
+            return self.adc3.read_adc(channel - 8, gain)
+        else:
+            return self.adc4.read_adc(channel - 12, gain)
--- floatsensor.py
+++ floatsensor.py
@@ -1,39 +1,36 @@
-import RPi.GPIO as GPIO
+from ADS1115 import ADS1115
 import time
 
 import sensor_repo as sr
 import utils
 
 CONTEXT = "vlotter"
-PIN = 36
+CHANNEL = 8
+THRESHOLD = 25000
 
 utils.setup_logging(CONTEXT)
 repo = sr.sensor_repo()
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN, GPIO.IN)
+ads = ADS1115()
 
 try:
-    utils.retry_if_none(lambda : repo.set_value(CONTEXT, "0"))
     high_level_cnt = 0;
     
     while True:
-        if GPIO.input(PIN) == 0:
+        value = ads.read( channel = CHANNEL )
+        if value < THRESHOLD:
             high_level_cnt += 1
         else:
-            time.sleep(5)
             utils.retry_if_none(lambda : repo.set_value(CONTEXT, "0"))                
             high_level_cnt = 0
+            time.sleep(5)
         
         if high_level_cnt > 100:
             utils.retry_if_none(lambda : repo.set_value(CONTEXT, "1"))             
-            high_level_cnt = 0           
+            high_level_cnt = 0
         
         time.sleep(0.1)    
 except KeyboardInterrupt:
     pass
 except:
     logging.exception(f"[{CONTEXT}] general error")
-finally:
-    GPIO.cleanup(PIN)
     
